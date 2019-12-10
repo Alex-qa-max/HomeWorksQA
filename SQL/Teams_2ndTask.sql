cc7dd16e9587c0eba255dd9ac407d3553a6289bb
Alex-qa-max <kozjemyaka@gmail.com>
2019-12-10 20:37:06 +0200

    Change SQL File Teams_2ndTask

--- SQL/Teams_2ndTask.sql
+++ SQL/Teams_2ndTask.sql
@@ -1,64 +1,64 @@
 --1--
-select c.name as Country, con.Name as Continent from Country as c, Continent as con where c.ContinentId=con.Id 
-Order by c.name DESC, con.name
+SELECT c.Name AS Country, con.Name AS Continent FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id 
+ORDER BY c.Name DESC, con.Name
 
 --2--
-select c.name as Country, con.Name as Continent from Country as c, Continent as con where c.ContinentId=con.Id AND c.Champion>0 ORDER by con.Name DESC,c.Name 
+SELECT c.Name AS Country, con.Name AS Continent FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Champion>0 ORDER BY con.Name DESC,c.Name 
 
 --3--
-select c.name as Country, l.name as Languages from Country as c, CountryLanguage as cl, Language as l where c.id=cl.CountryId AND cl.LanguageId=l.Id Order by l.name
+SELECT c.Name AS Country, l.Name AS Languages FROM Country AS c, CountryLanguage AS cl, Language AS l WHERE c.id=cl.CountryId AND cl.LanguageId=l.Id ORDER BY l.Name
 
 --4--
-select c.name from Country as c, Continent as con, CountryLanguage as cl, Language as l, Currency as cur where c.id=cl.CountryId AND cl.LanguageId=l.Id AND c.ContinentId=con.Id And cur.id=c.CurrencyId AND 
-con.Name IN ('Южная Америка', 'Центральная Америка', 'Северная Америка') AND cur.name != 'доллар США' AND l.name != 'испанский'
+SELECT c.Name FROM Country AS c, Continent AS con, CountryLanguage AS cl, Language AS l, Currency AS cur WHERE c.id=cl.CountryId AND cl.LanguageId=l.Id AND c.ContinentId=con.Id AND cur.id=c.CurrencyId AND 
+con.Name IN ('Южная Америка', 'Центральная Америка', 'Северная Америка') AND cur.Name != 'доллар США' AND l.Name != 'испанский'
 
 --5--
-select c.name as Country, con.Name as Continent from Country as c, Continent as con where c.ContinentId=con.Id AND c.Debut>0
-Order by con.name, c.name DESC
+SELECT c.Name AS Country, con.Name AS Continent FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Debut>0
+ORDER BY con.Name, c.Name DESC
 
 --6--
-select c.name from Country as c, Continent as con, CountryLanguage as cl, Language as l  where c.id=cl.CountryId AND cl.LanguageId=l.Id AND c.ContinentId=con.Id And  
-con.Name ='Африка' AND  l.name = 'французский'
+SELECT c.Name FROM Country AS c, Continent AS con, CountryLanguage AS cl, Language AS l  WHERE c.id=cl.CountryId AND cl.LanguageId=l.Id AND c.ContinentId=con.Id AND  
+con.Name ='Африка' AND  l.Name = 'французский'
 
 --7--
-select c.name as Country from Country as c, Continent as con, Currency as cur where c.ContinentId=con.Id AND cur.id=c.CurrencyId AND  c.Champion>0 AND cur.Name='евро'
+SELECT c.Name AS Country FROM Country AS c, Continent AS con, Currency AS cur WHERE c.ContinentId=con.Id AND cur.id=c.CurrencyId AND  c.Champion>0 AND cur.Name='евро'
 
 --8--
-select c.name from Country as c, CountryLanguage as cl, Language as l  where cl.LanguageId=l.Id AND c.id=cl.CountryId And l.name = 'английский' And c.Debut>0
+SELECT c.Name FROM Country AS c, CountryLanguage AS cl, Language AS l  WHERE cl.LanguageId=l.Id AND c.id=cl.CountryId AND l.Name = 'английский' AND c.Debut>0
 
 --9--
-select c.name as Country, con.Name as Continent from Country as c, Continent as con, CountryLanguage as cl, Language as l  where c.id=cl.CountryId AND cl.LanguageId=l.Id AND c.ContinentId=con.Id And  
-  l.name = 'английский' order by c.name 
+SELECT c.Name AS Country, con.Name AS Continent FROM Country AS c, Continent AS con, CountryLanguage AS cl, Language AS l  WHERE c.id=cl.CountryId AND cl.LanguageId=l.Id AND c.ContinentId=con.Id AND  
+  l.Name = 'английский' ORDER BY c.Name 
 
 --10--
-select c.name as Country from Country as c, Continent as con where c.ContinentId=con.Id AND c.Champion>0 AND con.Name!='Европа'
+SELECT c.Name AS Country FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Champion>0 AND con.Name!='Европа'
 
 --11--
-select count(c.name) as Count_of_Countries from Country as c, CountryLanguage as cl, Language as l  where cl.LanguageId=l.Id AND c.id=cl.CountryId And l.name = 'английский'
+SELECT count(c.Name) AS Count_of_Countries FROM Country AS c, CountryLanguage AS cl, Language AS l  WHERE cl.LanguageId=l.Id AND c.id=cl.CountryId AND l.Name = 'английский'
 
 --12--
-select count(c.name) as Count_of_Countries from Country as c, Currency as cur  where cur.id=c.CurrencyId AND cur.Name='евро'
+SELECT count(c.Name) AS Count_of_Countries FROM Country AS c, Currency AS cur  WHERE cur.id=c.CurrencyId AND cur.Name='евро'
 
 --13--
-select count(c.name) as Count_of_Countries from Country as c, Continent as con where c.ContinentId=con.Id AND c.Champion>0 
+SELECT count(c.Name) AS Count_of_Countries FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Champion>0 
 
 --14--
-select con.Name  as Continent, AVG(c.Territory) as AVG_Territory  from Country as c, Continent as con where c.ContinentId=con.Id Group by con.Name
+SELECT con.Name  AS Continent, AVG(c.Territory) AS AVG_Territory  FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id GROUP BY con.Name
 
 --15--
-select SUM(c.Champion) as Count_of_Countries from Country as c, Continent as con where c.ContinentId=con.Id AND c.Champion>0
+SELECT SUM(c.Champion) AS Count_of_Countries FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Champion>0
 
 --16--
-select c.name as Country, SUM(c.Champion) as Count_of_Countries from Country as c, Continent as con where c.ContinentId=con.Id AND c.Champion>0 group by c.Name
+SELECT c.Name AS Country, SUM(c.Champion) AS Count_of_Countries FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Champion>0 GROUP BY c.Name
 
 --17--
-select  count(c.name) as Count_of_Countries from Country as c, Continent as con where c.ContinentId=con.Id AND con.Name IN ('Европа','Азия')
+SELECT  count(c.Name) AS Count_of_Countries FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND con.Name IN ('Европа','Азия')
 
 --18--
-select c.Name  as Country, c.Territory  from Country as c, Continent as con where c.ContinentId=con.Id AND c.Territory=(select MIN(Territory) from Country)
+SELECT c.Name  AS Country, c.Territory  FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Territory=(SELECT MIN(Territory) FROM Country)
 
 --19--
-select count(c.name) as Count_of_Countries from Country as c, Continent as con where c.ContinentId=con.Id AND c.Debut>0 
+SELECT count(c.Name) AS Count_of_Countries FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id AND c.Debut>0 
 
 --20--
-select count(c.name) as Count_of_Countries from Country as c, Continent as con where c.ContinentId=con.Id
+SELECT count(c.Name) AS Count_of_Countries FROM Country AS c, Continent AS con WHERE c.ContinentId=con.Id
